0d481eb401c1ad61b1186d91fc3ccb42f129dd66
Ben Humphries <ben_humphries@caryacademy.org>
2017-07-07 09:26:37 -0400

    Started adding a way to save comments that have already been replied to
    It's not working right now cause I dont know how files and lists work in
    python. Will fix after I eat some food.

--- IDontSeeItBot.py
+++ IDontSeeItBot.py
@@ -1,14 +1,17 @@
 import praw as pr
 import urllib.request
 import re
+import csv
 from PIL import Image
 from imgurpython import ImgurClient as ip
 
 from credentials import *
 
 
+#Combine and Upload Images
+#Parameters: Reddit comment object, path to first image, path to second image
 def combine_and_upload(comment, path1, path2):
-	#Combine Images
+	#Combine images
 	image1 = Image.open(path1)
 	image2 = Image.open(path2)
 
@@ -31,36 +34,63 @@ def combine_and_upload(comment, path1, path2):
 	image = client.upload_from_path("final.png",config=config,anon=False)
 	print(image["link"])
 
+	#Submit a reply linking to the final image
 	comment.reply(image["link"])
 
+#Download Image From Submission
 def download_from_submission(submission):
-
+	#Download original post image
 	file_name, headers = urllib.request.urlretrieve(submission.url, "submission" + ".png")
 
+	#Return path to image
 	return file_name
 
+#Download Image From Comment
 def download_from_comment(comment):
-
+	#Find parent comment
 	while not comment.is_root:
 		comment = comment.parent()
 
+	if re.search("(?P<url>https?://[^\s]+)", comment.body) is None:
+		return "No Image"
+
+	#Parse comment for link to photoshopped image
 	url = re.search("(?P<url>https?://[^\s]+)", comment.body).group("url")
 
+	#Download photoshopped image
 	file_name, headers = urllib.request.urlretrieve(url, "comment" + ".png")
 
+	#Return path to image
 	return file_name
 
+################################################################
 
+#Open .csv file containing previously replied to comments
+with open("completedcomments.csv", 'r') as file:
+	reader = csv.reader(file, delimiter=',')
+	comments_list = list(reader)
+	print(comments_list)
+
+#Access reddit API
 reddit = pr.Reddit(client_id = reddit_client_id,
  client_secret = reddit_client_secret,
   user_agent = "Python:IDontSeeItBot:1.0.0 (by /u/panner2)",
   username = username,
   password = password)
 
+#Access subreddit
 subreddit = reddit.subreddit("bottest")
 
-#Search for "!IDontSeeIt"
+#Search for "!IDontSeeIt" in the top 25 "hot" posts
 for submission in subreddit.hot(limit=25):
 	for comment in submission.comments.list():
-		if("!idontseeit" in comment.body.lower()):
+		if("!idontseeit" in comment.body.lower() and comment.permalink not in comments_list and download_from_comment(comment) != "No Image"):
 			combine_and_upload(comment, download_from_submission(submission), download_from_comment(comment))
+			comments_list.append(comment.permalink(fast=False))
+
+#Save .csv file containing updated list of previously replied to comments
+with open("completedcomments.csv", 'w') as outfile:
+
+	writer = csv.writer(outfile, delimiter=',')
+	writer.writerow(comments_list)
+	print(comments_list)
